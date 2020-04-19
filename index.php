3fe2800eb4839fa6d4c7a7e081abf90f10020990
bird-coder <1037297660@qq.com>
2020-04-19 17:26:07 +0800

    防止重复请求

--- index.php
+++ index.php
@@ -7,12 +7,16 @@ $lib->$action();
 
 class index
 {
+    private $lockFile = './log/lock.txt';
+
     public function __construct()
     {
         Log::write('receive params: '.urldecode(json_encode($_REQUEST)));
     }
 
     public function choose() {
+        if ($this->isLock()) return false;
+        $this->lock();
         $mark = Util::param('mark');
         if (in_array($mark, ['like', 'change', 'ban'])) {
             $index = Util::param('id');
@@ -23,6 +27,21 @@ class index
                 }
             }
         }
-        echo '操作成功';
+        $this->unlock();
+        echo "<script>alert('操作成功');history.go(-1);</script>";
+    }
+
+    private function isLock() {
+        $time = intval(file_get_contents($this->lockFile));
+        if (time() < $time + 1) return true;
+        return false;
+    }
+
+    private function lock() {
+        file_put_contents($this->lockFile, time());
+    }
+
+    private function unlock() {
+        if (file_exists($this->lockFile)) unlink($this->lockFile);
     }
 }
